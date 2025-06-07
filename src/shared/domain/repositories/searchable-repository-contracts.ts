12322057473ee0dc7187fad047cacd3268a1fa86
Carlos Henrique de S. Ferreira <carloshenrique4m@gmail.com>
2025-06-07 11:39:24 -0300

    70. refactoring user repository pt1

--- src/shared/domain/repositories/searchable-repository-contracts.ts
+++ src/shared/domain/repositories/searchable-repository-contracts.ts
@@ -21,14 +21,14 @@ export type SearchResultProps<E extends Entity, Filter> = {
   filter: Filter | null
 }
 
-export class SearchParams {
+export class SearchParams<Filter = string> {
   protected _page: number
   protected _perPage: number = 10
   protected _sort: string | null
   protected _sortDir: SortDirection | null
-  protected _filter: string | null
+  protected _filter: Filter | null
 
-  constructor(props: SearchProps = {}) {
+  constructor(props: SearchProps<Filter> = {}) {
     this.page = props.page
     this.perPage = props.perPage
     this.sort = props.sort
@@ -82,12 +82,12 @@ export class SearchParams {
     this._sortDir = dir !== 'asc' && dir !== 'desc' ? 'desc' : dir
   }
 
-  get filter() {
+  get filter(): Filter | null {
     return this._filter
   }
 
-  set filter(value: string | null) {
-    this._filter = value === null || value === undefined || value === '' ? null : `${value}`
+  set filter(value: Filter | null) {
+    this._filter = value === null || value === undefined || value === '' ? null : (`${value}` as any)
   }
 }
 
@@ -129,7 +129,7 @@ export class SearchResult<E extends Entity, Filter = string> {
 export interface SearchableRepositoryInterface<
   E extends Entity,
   Filter = string,
-  SearchInput = SearchParams,
+  SearchInput = SearchParams<Filter>,
   SearchOutput = SearchResult<E, Filter>
 > extends RepositoryInterface<E> {
   sortableFields: string[]
