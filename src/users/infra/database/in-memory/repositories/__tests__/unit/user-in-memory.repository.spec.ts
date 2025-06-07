5992459c1d853741bbe86a820a35ec92013c9c40
Carlos Henrique de S. Ferreira <carloshenrique4m@gmail.com>
2025-06-07 12:11:57 -0300

    72. user in memory repository unit tests - findByEmail, emailExists

---
+++ src/users/infra/database/in-memory/repositories/__tests__/unit/user-in-memory.repository.spec.ts
@@ -0,0 +1,46 @@
+import { UserEntity } from '@/users/domain/entities/user.entity';
+import { UserInMemoryRepository } from '../../user-in-memory.repository';
+import { UserDataBuilder } from '@/users/domain/entities/testing/helpers/user-data-builder';
+import { NotFoundError } from '@/shared/domain/errors/not-found-error';
+import { ConflictError } from '@/shared/domain/errors/conflict-error';
+
+describe('InMemoryRepository - unit tests', () => {
+  let sut: UserInMemoryRepository;
+
+  beforeEach(() => {
+    sut = new UserInMemoryRepository();
+  });
+
+  describe('findByEmail method', () => {
+    it('should throw error when not found', async () => {
+      await expect(sut.findByEmail('a@a.com')).rejects.toThrow(
+        new NotFoundError('Entity not found using email: a@a.com'),
+      );
+    });
+
+    it('should find a entity by email', async () => {
+      const entity = new UserEntity(UserDataBuilder({}));
+
+      await sut.insert(entity);
+      const result = await sut.findByEmail(entity.email);
+
+      expect(entity.toJson()).toStrictEqual(result.toJson());
+    });
+  });
+
+  describe('emailExists method', () => {
+    it('should throw error when email exists', async () => {
+      const entity = new UserEntity(UserDataBuilder({}));
+      await sut.insert(entity);
+
+      await expect(sut.emailExists(entity.email)).rejects.toThrow(
+        new ConflictError('Email address already used'),
+      );
+    });
+
+    it('should not throw errors when email not exists', async () => {
+      expect.assertions(0);
+      await sut.emailExists('a@a.com');
+    });
+  });
+});
