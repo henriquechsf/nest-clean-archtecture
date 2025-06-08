57a71d1e5d9ef9fce0cf1e8a9ddc22ea6b6bf604
Carlos Henrique de S. Ferreira <carloshenrique4m@gmail.com>
2025-06-07 21:47:30 -0300

    80. refactor create user usecase for hash password

--- src/users/application/usecases/signup.usecase.ts
+++ src/users/application/usecases/signup.usecase.ts
@@ -1,6 +1,7 @@
 import { UserRepository } from "@/users/domain/repositories/user.repository"
 import { BadRequestError } from "../errors/bad-request-error"
 import { UserEntity } from "@/users/domain/entities/user.entity"
+import { HashProvider } from "@/shared/application/providers/hash-provider"
 
 export namespace SignupUseCase {
 
@@ -19,7 +20,10 @@ export namespace SignupUseCase {
   }
 
   export class UseCase {
-    constructor(private userRepository: UserRepository.Repository) {}
+    constructor(
+      private userRepository: UserRepository.Repository,
+      private hashProvider: HashProvider
+    ) {}
 
     async execute(input: Input): Promise<Output> {
       const { name, email, password } = input
@@ -30,7 +34,11 @@ export namespace SignupUseCase {
 
       await this.userRepository.emailExists(email)
 
-      const entity = new UserEntity(input)
+      const hashPassword = await this.hashProvider.generateHash(password)
+
+      const entity = new UserEntity(
+        Object.assign(input, { password: hashPassword })
+      )
 
       await this.userRepository.insert(entity)
 
