e29b7c5d58d2bebcf1ec75439aa6604e823eb00d
Carlos Henrique de S. Ferreira <carloshenrique4m@gmail.com>
2025-06-15 13:51:44 -0300

    107. user controller update - unit tests

--- src/users/infrastructure/controllers/__tests__/unit/users.controller.spec.ts
+++ src/users/infrastructure/controllers/__tests__/unit/users.controller.spec.ts
@@ -2,8 +2,10 @@ import { UsersController } from '../../users.controller';
 import { UserOutput } from '@/users/application/dtos/user-output';
 import { SigninUseCase } from '@/users/application/usecases/signin.usecase';
 import { SignupUseCase } from '@/users/application/usecases/signup.usecase';
+import { UpdateUserUseCase } from '@/users/application/usecases/update-user.usecase';
 import { SignInDto } from '@/users/infrastructure/dtos/signin.dto';
 import { SignUpDto } from '@/users/infrastructure/dtos/signup.dto';
+import { UpdateUserDto } from '@/users/infrastructure/dtos/update-user.dto';
 
 describe('UsersController - unit tests', () => {
   let sut: UsersController;
@@ -12,14 +14,14 @@ describe('UsersController - unit tests', () => {
 
   beforeEach(async () => {
     sut = new UsersController();
-    id = 'ff7dfdd7-e81e-4aa0-acd7-bd2e9c6001de'
+    id = 'ff7dfdd7-e81e-4aa0-acd7-bd2e9c6001de';
     props = {
       id,
       name: 'John Doe',
       email: 'john.doe@example.com',
       password: '1234',
-      createdAt: new Date()
-    }
+      createdAt: new Date(),
+    };
   });
 
   it('should be defined', () => {
@@ -27,41 +29,62 @@ describe('UsersController - unit tests', () => {
   });
 
   it('should create an user', async () => {
-    const output: SignupUseCase.Output = props
+    const output: SignupUseCase.Output = props;
     const mockSignupUseCase = {
-      execute: jest.fn().mockResolvedValue(output)
-    }
-    sut['signupUseCase'] = mockSignupUseCase as any
+      execute: jest.fn().mockResolvedValue(output),
+    };
+    sut['signupUseCase'] = mockSignupUseCase as any;
 
     const input: SignUpDto = {
       name: 'John Doe',
       email: 'john.doe@example.com',
-      password: '1234'
-    }
+      password: '1234',
+    };
 
     const result = await sut.create(input);
 
     expect(result).toMatchObject(output);
-    expect(mockSignupUseCase.execute).toHaveBeenCalledTimes(1)
-    expect(mockSignupUseCase.execute).toHaveBeenCalledWith(input)
+    expect(mockSignupUseCase.execute).toHaveBeenCalledTimes(1);
+    expect(mockSignupUseCase.execute).toHaveBeenCalledWith(input);
   });
 
   it('should authenticate an user', async () => {
-    const output: SigninUseCase.Output = props
+    const output: SigninUseCase.Output = props;
     const mockSigninUseCase = {
-      execute: jest.fn().mockResolvedValue(output)
-    }
-    sut['signinUseCase'] = mockSigninUseCase as any
+      execute: jest.fn().mockResolvedValue(output),
+    };
+    sut['signinUseCase'] = mockSigninUseCase as any;
 
     const input: SignInDto = {
       email: 'john.doe@example.com',
-      password: '1234'
-    }
+      password: '1234',
+    };
 
     const result = await sut.login(input);
 
     expect(result).toMatchObject(output);
-    expect(mockSigninUseCase.execute).toHaveBeenCalledTimes(1)
-    expect(mockSigninUseCase.execute).toHaveBeenCalledWith(input)
+    expect(mockSigninUseCase.execute).toHaveBeenCalledTimes(1);
+    expect(mockSigninUseCase.execute).toHaveBeenCalledWith(input);
+  });
+
+  it('should update an user', async () => {
+    const output: UpdateUserUseCase.Output = props;
+    const mockUpdateUserUseCase = {
+      execute: jest.fn().mockResolvedValue(output),
+    };
+    sut['updateUserUseCase'] = mockUpdateUserUseCase as any;
+
+    const input: UpdateUserDto = {
+      name: 'John Doe Edit',
+    };
+
+    const result = await sut.update(id, input);
+
+    expect(result).toMatchObject(output);
+    expect(mockUpdateUserUseCase.execute).toHaveBeenCalledTimes(1);
+    expect(mockUpdateUserUseCase.execute).toHaveBeenCalledWith({
+      id,
+      ...input,
+    });
   });
 });
