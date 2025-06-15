6cdac69282e5bdd9e097388eb848e010d4148bff
Carlos Henrique de S. Ferreira <carloshenrique4m@gmail.com>
2025-06-15 13:45:58 -0300

    106. user controller login - unit tests

--- src/users/infrastructure/controllers/__tests__/unit/users.controller.spec.ts
+++ src/users/infrastructure/controllers/__tests__/unit/users.controller.spec.ts
@@ -1,7 +1,8 @@
-import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from '../../users.controller';
 import { UserOutput } from '@/users/application/dtos/user-output';
+import { SigninUseCase } from '@/users/application/usecases/signin.usecase';
 import { SignupUseCase } from '@/users/application/usecases/signup.usecase';
+import { SignInDto } from '@/users/infrastructure/dtos/signin.dto';
 import { SignUpDto } from '@/users/infrastructure/dtos/signup.dto';
 
 describe('UsersController - unit tests', () => {
@@ -43,5 +44,24 @@ describe('UsersController - unit tests', () => {
     expect(result).toMatchObject(output);
     expect(mockSignupUseCase.execute).toHaveBeenCalledTimes(1)
     expect(mockSignupUseCase.execute).toHaveBeenCalledWith(input)
+  });
+
+  it('should authenticate an user', async () => {
+    const output: SigninUseCase.Output = props
+    const mockSigninUseCase = {
+      execute: jest.fn().mockResolvedValue(output)
+    }
+    sut['signinUseCase'] = mockSigninUseCase as any
+
+    const input: SignInDto = {
+      email: 'john.doe@example.com',
+      password: '1234'
+    }
+
+    const result = await sut.login(input);
+
+    expect(result).toMatchObject(output);
+    expect(mockSigninUseCase.execute).toHaveBeenCalledTimes(1)
+    expect(mockSigninUseCase.execute).toHaveBeenCalledWith(input)
   });
 });
