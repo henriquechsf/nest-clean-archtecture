a8eadef303c82b472d2724b281068bed839dec47
Carlos Henrique de S. Ferreira <carloshenrique4m@gmail.com>
2025-06-25 20:05:17 -0300

    119.  findById UserPrismaRepository

---
+++ src/users/infrastructure/database/prisma/repositories/user-prisma.repository.ts
@@ -0,0 +1,55 @@
+import { NotFoundError } from "@/shared/domain/errors/not-found-error";
+import { PrismaService } from "@/shared/infrastructure/database/prisma/prisma.service";
+import { UserEntity } from "@/users/domain/entities/user.entity";
+import { UserRepository } from "@/users/domain/repositories/user.repository";
+import { UserModelMapper } from "../models/user-model.mapper";
+
+export class UserPrismaRepository implements UserRepository.Repository {
+  sortableFields: string[];
+
+  constructor(private prismaService: PrismaService) {}
+
+  findById(id: string): Promise<UserEntity> {
+    return this._get(id);
+  }
+
+  findByEmail(email: string): Promise<UserEntity> {
+    throw new Error("Method not implemented.");
+  }
+
+  emailExists(email: string): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  search(props: UserRepository.SearchParams): Promise<UserRepository.SearchResult> {
+    throw new Error("Method not implemented.");
+  }
+
+  insert(entity: UserEntity): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  findAll(): Promise<UserEntity[]> {
+    throw new Error("Method not implemented.");
+  }
+
+  update(entity: UserEntity): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  delete(id: string): Promise<void> {
+    throw new Error("Method not implemented.");
+  }
+
+  protected async _get(id: string): Promise<UserEntity> {
+    try {
+      const user = await this.prismaService.user.findFirstOrThrow({
+        where: {id}
+      })
+
+      return UserModelMapper.toEntity(user);
+    } catch {
+      throw new NotFoundError(`User with id ${id} not found`)
+    }
+  }
+}
