d4f463454cee6deaf2165a3b468dcce1c07a9530
Carlos Henrique de S. Ferreira <carloshenrique4m@gmail.com>
2025-07-01 20:59:52 -0300

    152. user collection presenter and unit tests

--- src/users/infrastructure/controllers/users.controller.ts
+++ src/users/infrastructure/controllers/users.controller.ts
@@ -25,7 +25,7 @@ import { SignInDto } from '../dtos/signin.dto';
 import { ListUsersDto } from '../dtos/list-users.dto';
 import { UpdatePasswordDto } from '../dtos/update-password.dto';
 import { UserOutput } from '@/users/application/dtos/user-output';
-import { UserPresenter } from '../presenters/user.presenter';
+import { UserCollectionPresenter, UserPresenter } from '../presenters/user.presenter';
 
 @Controller('users')
 export class UsersController {
@@ -54,6 +54,10 @@ export class UsersController {
     return new UserPresenter(output);
   }
 
+  static listUsersToResponse(output: ListUsersUseCase.Output) {
+    return new UserCollectionPresenter(output)
+  }
+
   @Post()
   async create(@Body() signupDto: SignUpDto) {
     const output = await this.signupUseCase.execute(signupDto);
@@ -69,7 +73,8 @@ export class UsersController {
 
   @Get()
   async search(@Query() searchParams: ListUsersDto) {
-    return this.listUsersUseCase.execute(searchParams);
+    const output = await this.listUsersUseCase.execute(searchParams);
+    return UsersController.listUsersToResponse(output);
   }
 
   @Get(':id')
