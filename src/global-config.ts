1382714579333a7697ade5863bc5f8c09ea650a2
Carlos Henrique de S. Ferreira <carloshenrique4m@gmail.com>
2025-07-06 19:44:26 -0300

    160. user http validation pipe

--- src/global-config.ts
+++ src/global-config.ts
@@ -1,11 +1,24 @@
-
 import { Reflector } from '@nestjs/core';
-import { ClassSerializerInterceptor, INestApplication } from '@nestjs/common';
+import {
+  ClassSerializerInterceptor,
+  HttpStatus,
+  INestApplication,
+  ValidationPipe,
+} from '@nestjs/common';
 import { WrapperDataInterceptor } from './shared/infrastructure/interceptors/wrapper-data/wrapper-data.interceptor';
 
 export function applyGlobalConfig(app: INestApplication) {
+  app.useGlobalPipes(
+    new ValidationPipe({
+      errorHttpStatusCode: HttpStatus.UNPROCESSABLE_ENTITY,
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
   app.useGlobalInterceptors(
     new WrapperDataInterceptor(),
-    new ClassSerializerInterceptor(app.get(Reflector))
-  )
+    new ClassSerializerInterceptor(app.get(Reflector)),
+  );
 }
--- src/users/infrastructure/dtos/signin.dto.ts
+++ src/users/infrastructure/dtos/signin.dto.ts
@@ -1,6 +1,13 @@
-import { SigninUseCase } from "@/users/application/usecases/signin.usecase";
+import { SigninUseCase } from '@/users/application/usecases/signin.usecase';
+import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
 
 export class SignInDto implements SigninUseCase.Input {
+  @IsString()
+  @IsNotEmpty()
+  @IsEmail()
   email: string;
+
+  @IsString()
+  @IsNotEmpty()
   password: string;
 }
