530e1c763acb611a542224dac825f01d51db0cee
Carlos Henrique de S. Ferreira <carloshenrique4m@gmail.com>
2025-07-07 23:39:36 -0300

    177. install jwt and add env configs

--- src/shared/infrastructure/env-config/env-config.service.ts
+++ src/shared/infrastructure/env-config/env-config.service.ts
@@ -4,14 +4,21 @@ import { ConfigService } from '@nestjs/config';
 
 @Injectable()
 export class EnvConfigService implements EnvConfig {
-
   constructor(private configService: ConfigService) {}
 
   getAppPort(): number {
-    return Number(this.configService.get<number>('PORT'))
+    return Number(this.configService.get<number>('PORT'));
   }
 
   getNodeEnv(): string {
-    return this.configService.get<string>('NODE_ENV')
+    return this.configService.get<string>('NODE_ENV');
+  }
+
+  getJwtSecret(): string {
+    return this.configService.get<string>('JWT_SECRET');
+  }
+
+  getJwtExpiresInSeconds(): number {
+    return Number(this.configService.get<number>('JWT_EXPIRES_IN'));
   }
 }
