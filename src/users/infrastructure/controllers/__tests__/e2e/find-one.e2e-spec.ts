fd486f758b278af0c168a92eeef8d67c6a01a6c6
Carlos Henrique de S. Ferreira <carloshenrique4m@gmail.com>
2025-07-18 23:07:41 -0300

    187. refactoring user find-one controller - e2e tests

--- src/users/infrastructure/controllers/__tests__/e2e/find-one.e2e-spec.ts
+++ src/users/infrastructure/controllers/__tests__/e2e/find-one.e2e-spec.ts
@@ -12,6 +12,8 @@ import { instanceToPlain } from 'class-transformer';
 import { applyGlobalConfig } from '@/global-config';
 import { UserEntity } from '@/users/domain/entities/user.entity';
 import { UserDataBuilder } from '@/users/domain/entities/testing/helpers/user-data-builder';
+import { HashProvider } from '@/shared/application/providers/hash-provider';
+import { BcryptjsHashProvider } from '@/shared/infrastructure/providers/hash-providers/bcryptjs-hash.provider';
 
 describe('UsersController - e2e tests', () => {
   let app: INestApplication;
@@ -19,6 +21,9 @@ describe('UsersController - e2e tests', () => {
   let repository: UserRepository.Repository;
   const prismaService = new PrismaClient();
   let entity: UserEntity;
+  let hashProvider: HashProvider;
+  let hashPassword: string;
+  let accessToken: string;
 
   beforeAll(async () => {
     setupPrismaTests();
@@ -35,6 +40,8 @@ describe('UsersController - e2e tests', () => {
     await app.init();
 
     repository = module.get<UserRepository.Repository>('UserRepository');
+    hashProvider = new BcryptjsHashProvider();
+    hashPassword = await hashProvider.generateHash('1234');
   });
 
   afterAll(async () => {
@@ -43,14 +50,27 @@ describe('UsersController - e2e tests', () => {
 
   beforeEach(async () => {
     await prismaService.user.deleteMany();
-    entity = new UserEntity(UserDataBuilder({}));
+    entity = new UserEntity(
+      UserDataBuilder({
+        email: 'a@a.com',
+        password: hashPassword,
+      }),
+    );
     await repository.insert(entity);
+
+    const loginResponse = await request(app.getHttpServer())
+      .post(`/users/login`)
+      .send({ email: 'a@a.com', password: '1234' })
+      .expect(200);
+
+    accessToken = loginResponse.body.accessToken;
   });
 
   describe('GET /users/:id', () => {
     it('should get a user', async () => {
       const res = await request(app.getHttpServer())
         .get(`/users/${entity.id}`)
+        .set('Authorization', `Bearer ${accessToken}`)
         .expect(200);
 
       expect(Object.keys(res.body)).toStrictEqual(['data']);
@@ -64,11 +84,22 @@ describe('UsersController - e2e tests', () => {
     it('should return an error with 404 code when throw NotFoundError with invalid id', async () => {
       await request(app.getHttpServer())
         .get(`/users/fakeid`)
+        .set('Authorization', `Bearer ${accessToken}`)
         .expect(404)
         .expect({
           statusCode: 404,
           error: 'Not Found',
           message: 'User with id fakeid not found',
+        });
+    });
+
+    it('should return an error with 401 code when request is not authorized', async () => {
+      await request(app.getHttpServer())
+        .get(`/users/fakeid`)
+        .expect(401)
+        .expect({
+          statusCode: 401,
+          message: 'Unauthorized',
         });
     });
   });
